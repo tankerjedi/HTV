1f49804ff452de8dd799d11f4d321051c9dfb35c
Tanker Jedi <tankerjedi@citromail.hu>
2012-12-08 15:40:08 +0100

    attach space and company object

--- main.cpp
+++ main.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 //Company class
-#include "company.h"
+//#include "company.h"
 #include "space.h"
 
 int main(int argc, char * argv[])
 {
 
+	space budapest;	
+
 	printf("Alpha verzió\n Hotelling szimulációs program \n");
 
 	company vallalat;
-	vallalat.x = 10;
-	vallalat.y = 20;
-	printf("A vállalat elhelyezkedése: (%d,%d)\n",vallalat.x,vallalat.y);
+	budapest.a.x = 10;
+	budapest.a.y = 20;
+	printf("A vállalat elhelyezkedése: (%d,%d)\n",budapest.a.x,budapest.a.y);
 	
-	vallalat.jobbralep();
-	printf("A vállalat elhelyezkedése: (%d,%d)\n",vallalat.x,vallalat.y);
+	budapest.a.jobbralep();
+	printf("A vállalat elhelyezkedése: (%d,%d)\n",budapest.a.x,budapest.a.y);
 
-	space ter;
 
-	printf("%d\n",ter.points[3][0][0]);
+	printf("%d\n",budapest.points[3][0][0]);
 
 	return 0;
 }
--- space.h
+++ space.h
@@ -1,11 +1,17 @@
 //Space class
 
+#include "company.h"
+
+//Definition
+
 class space
 {
 	public:
 		int points[10][1][1];
 		int number_of_inhabitant;
-		
+		company a;
+		company b;
+
 		space();
 		void new_company();
 
